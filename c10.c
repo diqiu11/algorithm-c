7a2afba506bf7dcdb373e0f6c5cb5ba11b26a62e
linuxdiqiu <di15218143233@outlook.com>
2018-10-31 23:10:25 +0800

    10.31/1

--- c10.c
+++ c10.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
 #define M 99999
-int main()
+int main()//待改
 {
-    long num;
+    int num;
     scanf("%d",&num);
     if(num>M){
-        printf("error!")
+        printf("error!");
     }else{
         int i,j,k;
         for(i=0;i<=4;i++){
-            if((int)(num/10)==1){
+            if((int)(num/10)==0){
                 j=i;
                 break;
             }
         }
-        printf("program have %d row.",j);
+        printf("program have %d row.\n",j);
         int s=0;
-        double num_postion[6];
+        float num_postion[6];
         for(k=10;k<=10000;k*10){
             num_postion[s]=num/k;
-            printf("%0.1f",num_postion[s]);
+            printf("%0.1f\n",num_postion[s]);
             s+=1;
             if(num_postion[s]<1){
                 break;
             }
         }
-        int n;
+        float n;
+        int q;
         for(q=6;q>=1;q--){
             n=num_postion[q];
             //排头遇到零
-            printf("%0.1",n);
+            printf("%0.1f\n",n);
         }
     }
     return 0;
